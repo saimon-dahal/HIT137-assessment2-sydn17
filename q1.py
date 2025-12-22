da904d8012fa9ae99ef5ef9f232058d049ce3671
saimon-dahal <saimondahal66@gmail.com>
2025-12-22 14:25:52 +1100

    feat: encryption function for raw text contents - reads from raw text file - promptes the user for shift values - encrypts the contents based on various predefined rules - saves the encrypted text in encrypted_text.txt file

---
+++ q1.py
@@ -0,0 +1,53 @@
+def encrypt_character(character, shift1, shift2):
+    # ASCII values for 'a' and 'A'
+    a = ord('a')
+    A = ord('A')
+    if character.islower():
+        # current position of our character from lowercase 'a'
+        position = ord(character) - a
+        if character <= 'm':
+            position += shift1 * shift2
+        else:
+            position -= shift1 + shift2
+        
+        # to have the position stay between the range 0-25
+        position = position % 26
+        shifted_character = chr(a + position)
+    
+    elif character.isupper():
+        # current position of our character from lowercase 'A'
+        position = ord(character) - A
+        if character <= 'M':
+            position -= shift1
+        else:
+            position += shift2 ** 2
+        position = position % 26
+        shifted_character = chr(A + position)
+
+    else:
+        # ignore non alphabetical characters
+        shifted_character = character 
+    
+    return shifted_character
+
+
+def encrypt_file(shift1, shift2):
+    with open("raw_text.txt", "r") as f:
+        file_contents = f.read()
+
+    encrypted =  ""
+
+    for character in file_contents:
+        encrypted += encrypt_character(character, shift1, shift2)
+
+    with open("encrypted_text.txt", "w") as f:
+        f.write(encrypted)
+
+def main():
+    shift1 = int(input("Enter shift1: "))
+    shift2 = int(input("Enter shift2: "))
+
+    encrypt_file(shift1=shift1, shift2=shift2)
+
+main()
+
